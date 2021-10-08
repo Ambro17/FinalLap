4a96bf02a0e4bdc79166259eadf47337093921af
nambrosini <nambrosini@onapsis.com>
2021-10-08 14:12:56 -0300

    🖌 Style improvements

--- solution/particle.py
+++ solution/particle.py
@@ -1,43 +1,52 @@
-import random
 from typing import Type
-
+from pprint import pprint
 from deap import base
 from deap import creator
+
 
 class Particle:
     def __init__(self, position, speed):
         self.position = position
         self.speed = speed
 
-
-def create_individual(creator_class: Type[Particle], position, speed):
-    return creator_class(position=position, speed=speed)
-
-
-creator.create("FitnessMax", base.Fitness, weights=(1.0, ))
-creator.create(
-    "Particle", 
-    Particle,
-    fitness=creator.FitnessMax,
-    __str__=lambda self: f'Particle(speed={self.speed}, position={self.position})')
+    def __repr__(self):
+        return f'{self.__class__.__name__!s}(position={self.position}, speed={self.speed})'
 
 
-def initParticle(particleclass, max_speed, min_position, max_position):
-    """Creates particle from defined parameters"""
-    particle = particleclass()
-    particle.speed = random.randint(1, max_speed)
-    particle.position = random.randint(min_position, max_position)
-    return particle
+def initialize_entities():
+    """Create entities required for the genetic algorithm
 
-toolbox = base.Toolbox()
-toolbox.register("particle", create_individual, creator.Particle)
+    - Individual with fitness, speed and position attributes
+    - Function that creates individuals given a set of attributes (population generator)
+    """
+    toolbox = base.Toolbox()
+
+    # Create Particle class with fitness attribute to save individual aptitude
+    creator.create("FitnessMax", base.Fitness, weights=(1.0, ))
+    creator.create(
+        "Particle",
+        Particle,
+        fitness=creator.FitnessMax,
+        __str__=lambda self: f'Particle(speed={self.speed}, position={self.position})')
 
 
-p = toolbox.particle(position=0, speed=1)
-print(p)
+    # Create function that dynamically creates individuals of a population based on same criteria
+    def create_individual(creator_class: Type[Particle], position, speed):
+        return creator_class(position=position, speed=speed)
+
+    toolbox.register("particle", create_individual, creator.Particle)
+
+    return toolbox
+
+toolbox = initialize_entities()
+population = [toolbox.particle(position=x, speed=1) for x in range(10)]
+print('Population:', end=' \n')
+pprint(population)
+p = population[0]
+print('Individual: ', p)
 p.fitness.values = (1, )
 assert p.fitness.valid is True
 del p.fitness.values
 assert p.fitness.valid is False
 p.fitness.values = (10, )
-print(p.fitness)
+print('Fitness: ', p.fitness)
