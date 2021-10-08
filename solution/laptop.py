f921998be713aebe92300e392ab5c2a7b3ecca5a
nambrosini <nambrosini@onapsis.com>
2021-10-08 12:29:03 -0300

    🖌 Improve style

--- solution/laptop.py
+++ solution/laptop.py
@@ -23,6 +23,9 @@ class Laptop:
     display_size: float
     brand: Optional[str] = 'ANY'
 
+    def __str__(self) -> str:
+        return self.name
+
 
 def evaluate_fitness(laptop: Laptop, ideal_laptop: Laptop) -> int:
     db = get_database()
@@ -64,19 +67,22 @@ def best_match():
     my_ideal_laptop = Laptop(name='ideal', price=2500, autonomy_in_hours=10, weight=2, display_size=15)
 
     # Freeze the argument of the ideal laptop in the evaluate function
-    # So that it takes only one argument, as expected by deap
+    # So that it takes only one argument, as expected by deap library
     evaluate = functools.partial(evaluate_fitness, ideal_laptop=my_ideal_laptop)
 
     bad_laptop = Laptop(name='bad', price=1000, autonomy_in_hours=5, weight=1.5, display_size=12.3)
     good_laptop = Laptop(name='good', price=1500, autonomy_in_hours=7, weight=3, display_size=17)
     cheap_laptop = Laptop(name='cheap', price=500, autonomy_in_hours=11, weight=1, display_size=15)
     expensive_laptop = Laptop(name='expensive', price=2000, autonomy_in_hours=6, weight=3, display_size=15)
+ 
     print('Bad Laptop score:', evaluate(bad_laptop))
     print('Good Laptop score:', evaluate(good_laptop))
     print('Cheap Laptop score:', evaluate(cheap_laptop))
     print('Expensive Laptop score:', evaluate(expensive_laptop))
+ 
     laptops = [bad_laptop, good_laptop, cheap_laptop, expensive_laptop]
     sorted_laptops = sorted(laptops, key=evaluate)
+    print('Sorted Laptops by score', list(map(str, sorted_laptops)))
     print('Best Match: ', sorted_laptops[0])
 
 best_match()
