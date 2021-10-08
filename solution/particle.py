b16a810080da173484ee199ad9a7536fa12ac147
nambrosini <nambrosini@onapsis.com>
2021-10-08 13:54:43 -0300

    🚩 Checkpoint before replacing by custom class

--- solution/particle.py
+++ solution/particle.py
@@ -3,21 +3,31 @@ import random
 from deap import base
 from deap import creator
 
-creator.create("FitnessMax", base.Fitness, weights=(1.0, 1.0))
+class Particle:
+    def __init__(self, position, speed):
+        self.position = position
+        self.speed = speed
+
+
+def create_individual(creator_class, position, speed):
+    return creator_class(position=position, speed=speed)
+
+
+creator.create("FitnessMax", base.Fitness, weights=(1.0, ))
 creator.create(
     "Particle", 
     list, 
     fitness=creator.FitnessMax, 
     speed=None,
     position=None,
-    __str__=lambda self: f'Particle(speed={self.speed:.2f}, position={self.position})')
+    __str__=lambda self: f'Particle(speed={self.speed}, position={self.position})')
 
 def initParticle(particleclass, max_speed, min_position, max_position):
     """Creates particle from defined parameters"""
-    part = particleclass()
-    part.speed = random.uniform(1, max_speed)
-    part.position = random.randint(min_position, max_position)
-    return part
+    particle = particleclass()
+    particle.speed = random.randint(1, max_speed)
+    particle.position = random.randint(min_position, max_position)
+    return particle
 
 toolbox = base.Toolbox()
 toolbox.register("particle", initParticle, creator.Particle)
