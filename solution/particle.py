9386668ff71ff7a3155428d6aea357440b52df83
nambrosini <nambrosini@onapsis.com>
2021-10-08 14:02:07 -0300

    🏁 Replaced by custom class

--- solution/particle.py
+++ solution/particle.py
@@ -1,4 +1,5 @@
 import random
+from typing import Type
 
 from deap import base
 from deap import creator
@@ -9,18 +10,17 @@ class Particle:
         self.speed = speed
 
 
-def create_individual(creator_class, position, speed):
+def create_individual(creator_class: Type[Particle], position, speed):
     return creator_class(position=position, speed=speed)
 
 
 creator.create("FitnessMax", base.Fitness, weights=(1.0, ))
 creator.create(
     "Particle", 
-    list, 
-    fitness=creator.FitnessMax, 
-    speed=None,
-    position=None,
+    Particle,
+    fitness=creator.FitnessMax,
     __str__=lambda self: f'Particle(speed={self.speed}, position={self.position})')
+
 
 def initParticle(particleclass, max_speed, min_position, max_position):
     """Creates particle from defined parameters"""
@@ -30,8 +30,14 @@ def initParticle(particleclass, max_speed, min_position, max_position):
     return particle
 
 toolbox = base.Toolbox()
-toolbox.register("particle", initParticle, creator.Particle)
+toolbox.register("particle", create_individual, creator.Particle)
 
 
-p = toolbox.particle(max_speed=2, min_position=-3, max_position=3)
+p = toolbox.particle(position=0, speed=1)
 print(p)
+p.fitness.values = (1, )
+assert p.fitness.valid is True
+del p.fitness.values
+assert p.fitness.valid is False
+p.fitness.values = (10, )
+print(p.fitness)
